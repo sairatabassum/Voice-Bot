9ad6bee7598561f3d11335656b51039c6e3a65c5
saira <sairatabassum386@gmail.com>
2021-04-01 02:28:15 +0600

    commit

---
+++ ChatBot.py
@@ -0,0 +1,14 @@
+from tkinter import*
+
+
+
+
+win=Tk()
+win.geometry('400x550+400+15')
+win.title("Pikachu ChatBot")
+win.resizable(False,False)
+win.iconbitmap(r'chabot.ico')
+
+
+
+win.mainloop()
